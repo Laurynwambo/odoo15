8ef4a45d2e07172a5657f9a091b6787c36c800ef
Dayud Farah <dayudfarah@Dayuds-MacBook-Pro.local>
2022-11-10 11:36:39 +0300

    changed the import

--- custom-addons/odoo_saas_kit/models/saas_plan.py
+++ custom-addons/odoo_saas_kit/models/saas_plan.py
@@ -9,7 +9,8 @@
 from urllib.parse import urlparse
 from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
-from lib import containers
+# from . lib import containers
+import containers from lib
 from odoo.modules.module import get_module_resource
 from odoo.models import NewId
 from . lib import query
