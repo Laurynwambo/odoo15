ebb9ced75e52c0be75c4358628667c15be661955
Kadweka Katana <Kadweka>
2022-11-25 17:24:59 +0300

    updated the module

--- custom-addons/odoo-authentication/models/partner.py
+++ custom-addons/odoo-authentication/models/partner.py
@@ -3,11 +3,11 @@ from odoo import models, fields, api,_
 
 class PartnerExtension(models.Model):
     _inherit = 'res.partner'
-    
+
     password = fields.Text(string="partner password")
     access_token_ids = fields.One2many(
         string='Access Tokens',
         comodel_name='jwt_provider.access_token',
         inverse_name='partner_id',
     )
-    # otp=fields.Char(string='Otp')
+    otp=fields.Char(string='Otp')
