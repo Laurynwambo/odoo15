e3ac9e0846a810d8cbbab5e796e5dffbe503000e
Kadweka Katana <Kadweka>
2022-11-25 17:26:47 +0300

    changed the module

--- custom-addons/odoo-authentication/models/partner.py
+++ custom-addons/odoo-authentication/models/partner.py
@@ -5,9 +5,5 @@ class PartnerExtension(models.Model):
     _inherit = 'res.partner'
 
     password = fields.Text(string="partner password")
-    access_token_ids = fields.One2many(
-        string='Access Tokens',
-        comodel_name='jwt_provider.access_token',
-        inverse_name='partner_id',
-    )
+    access_token_ids = fields.One2many(string='Access Tokens',comodel_name='jwt_provider.access_token',inverse_name='partner_id')
     otp=fields.Char(string='Otp')
